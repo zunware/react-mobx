21994247dae5bc502e97ccfffb1aef1c0335e9a4
zunware <jose@josellausas.com>
2019-11-26 15:24:55 -0600

    OK

--- src/components/CountButton.tsx
+++ src/components/CountButton.tsx
@@ -1,23 +1,30 @@
 import * as React from 'react';
 import { observable, action } from "mobx";
+import { observer } from 'mobx-react';
 
-export class CountButton extends React.Component<{},{count:number}> {
+class CountData {
+  @observable count = 0;
+
+  @action
+  increment() {
+    this.count++;
+  }
+}
+
+@observer
+export class CountButton extends React.Component<{}> {
+  data = new CountData();
   constructor(props: any) {
     super(props);
     this.state = {
       count: 0
     };
   }
-  increment(){
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
   render() {
     return (
       <div>
-        <button onClick={() => this.increment()}>
-        {this.state.count}
+        <button onClick={() => this.data.increment()}>
+        {this.data.count}
         </button>
       </div>
     );
